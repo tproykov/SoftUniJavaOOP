3182599fa8649be3383c227759215378392d51d2
tproykov <tproykov@gmail.com>
2025-02-04 23:38:12 +0000

    Create L01RhombusOfStars.java

---
+++ src/JO01WorkingWithAbstractions/L01RhombusOfStars.java
@@ -0,0 +1,13 @@
+package JO01WorkingWithAbstractions;
+
+import java.util.Scanner;
+
+public class L01RhombusOfStars {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
