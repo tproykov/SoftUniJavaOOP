e8ab02513928e424475b80dc8cf1be277c95875f
tproykov <tproykov@gmail.com>
2025-02-04 23:42:04 +0000

    update

--- src/JO01WorkingWithAbstractions/L04HotelReservation/Main.java
+++ src/JO01WorkingWithAbstractions/L04HotelReservation/Main.java
@@ -1,4 +1,13 @@
 package JO01WorkingWithAbstractions.L04HotelReservation;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
 }
