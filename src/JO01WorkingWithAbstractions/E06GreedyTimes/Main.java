e4d121a9fdc0846bd4d7720f509556e28baeb9a0
tproykov <tproykov@gmail.com>
2025-02-04 23:47:09 +0000

    update

--- src/JO01WorkingWithAbstractions/E06GreedyTimes/Main.java
+++ src/JO01WorkingWithAbstractions/E06GreedyTimes/Main.java
@@ -1,4 +1,13 @@
 package JO01WorkingWithAbstractions.E06GreedyTimes;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
