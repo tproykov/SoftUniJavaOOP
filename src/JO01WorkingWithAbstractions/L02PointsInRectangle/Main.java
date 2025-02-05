c750938ca394c08978da5224566429dd34f8c55b
tproykov <tproykov@gmail.com>
2025-02-05 01:07:58 +0000

    Update Main.java

--- src/JO01WorkingWithAbstractions/L02PointsInRectangle/Main.java
+++ src/JO01WorkingWithAbstractions/L02PointsInRectangle/Main.java
@@ -1,5 +1,6 @@
 package JO01WorkingWithAbstractions.L02PointsInRectangle;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +8,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        int[] coordinates = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int x1 = coordinates[0];
+        int y1 = coordinates[1];
+        int x2 = coordinates[2];
+        int y2 = coordinates[3];
 
 
     }
