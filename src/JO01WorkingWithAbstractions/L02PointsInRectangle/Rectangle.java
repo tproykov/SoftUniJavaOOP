4716c44c09e3a7e6d5e4f46b3306e140fcff99b4
tproykov <tproykov@gmail.com>
2025-02-05 21:33:22 +0000

    Update Rectangle.java

--- src/JO01WorkingWithAbstractions/L02PointsInRectangle/Rectangle.java
+++ src/JO01WorkingWithAbstractions/L02PointsInRectangle/Rectangle.java
@@ -2,5 +2,13 @@ package JO01WorkingWithAbstractions.L02PointsInRectangle;
 
 public class Rectangle {
 
-    
+    private Point bottomLeft;
+    private Point TopRight;
+
+    public Rectangle(Point bottomLeft, Point TopRight) {
+        this.bottomLeft = bottomLeft;
+        this.TopRight = TopRight;
+    }
+
+    public boolean contains(Point point) {
 }
