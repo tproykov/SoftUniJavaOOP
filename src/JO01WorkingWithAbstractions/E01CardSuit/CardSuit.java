a1242c68cc557c1c333f31a31cf7496bdfe8416b
tproykov <tproykov@gmail.com>
2025-02-15 21:16:03 +0000

    update

---
+++ src/JO01WorkingWithAbstractions/E01CardSuit/CardSuit.java
@@ -0,0 +1,15 @@
+package JO01WorkingWithAbstractions.E01CardSuit;
+
+public enum CardSuit {
+
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES;
+
+
+    @Override
+    public String toString() {
+        return String.format("Ordinal value: %d; Name value: %s", this.ordinal(), this.name());
+    }
+}
--- src/JO01WorkingWithAbstractions/E01CardSuit/Main.java
+++ src/JO01WorkingWithAbstractions/E01CardSuit/Main.java
@@ -1,13 +1,13 @@
 package JO01WorkingWithAbstractions.E01CardSuit;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-
+        System.out.println("Card Suits:");
+        Arrays.stream(CardSuit.values())
+                .forEach(System.out::println);
     }
 }
