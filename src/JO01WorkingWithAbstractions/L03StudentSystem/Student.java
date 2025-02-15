ef3d94829dc584c6717aa78971ac1b6f4745ac49
tproykov <tproykov@gmail.com>
2025-02-15 15:00:52 +0000

    update

--- src/JO01WorkingWithAbstractions/L03StudentSystem/Student.java
+++ src/JO01WorkingWithAbstractions/L03StudentSystem/Student.java
@@ -15,23 +15,11 @@ public class Student {
         return this.name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return this.age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public double getGrade() {
         return this.grade;
-    }
-
-    public void setGrade(double grade) {
-        this.grade = grade;
     }
 }
