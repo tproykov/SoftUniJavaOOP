889a7a9e0194703144423e46cd4a0e8fe6c2aa06
tproykov <tproykov@gmail.com>
2025-02-15 18:56:21 +0000

    Update Main.java

--- src/JO01WorkingWithAbstractions/L04HotelReservation/Main.java
+++ src/JO01WorkingWithAbstractions/L04HotelReservation/Main.java
@@ -8,6 +8,12 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] input = scanner.nextLine().split(" ");
+        double pricePerDay = Double.parseDouble(input[0]);
+        int numberOfDays = Integer.parseInt(input[1]);
+        Season season = Season.valueOf(input[2]);
+        Discount discount = Discount.valueOf(input[3]);
+
 
     }
 }
