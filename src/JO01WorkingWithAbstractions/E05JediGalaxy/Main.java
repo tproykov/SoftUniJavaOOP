6b901d4895ebe340136063c9db4d94a5ee235afe
tproykov <tproykov@gmail.com>
2025-02-16 09:13:23 +0000

    update

--- src/JO01WorkingWithAbstractions/E05JediGalaxy/Main.java
+++ src/JO01WorkingWithAbstractions/E05JediGalaxy/Main.java
@@ -4,39 +4,45 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        int[] dimestions = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int x = dimestions[0];
-        int y = dimestions[1];
+        int[] dimensions = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        int[][] matrix = new int[x][y];
+        int rows = dimensions[0];
+        int cols = dimensions[1];
 
-        int value = 0;
-        for (int i = 0; i < x; i++)
-        {
-            for (int j = 0; j < y; j++)
-            {
-                matrix[i][j] = value++;
-            }
-        }
+        int[][] matrix = new int[rows][cols];
 
-        String command = scanner.nextLine();
+        fillTheMatrix(rows, cols, matrix);
+
         long sum = 0;
-        while (!command.equals("Let the Force be with you"))
-        {
-            int[] ivoS = Arrays.stream(command.split(" ")).mapToInt(Integer::parseInt).toArray();
-            int[] evil = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        String command;
+        while (!"Let the Force be with you".equals(command = scanner.nextLine())) {
+
+            int[] ivoS = Arrays.stream(command.split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            int[] evil = Arrays.stream(scanner.nextLine()
+                    .split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
             int xE = evil[0];
             int yE = evil[1];
 
-            while (xE >= 0 && yE >= 0)
-            {
-                if (xE >= 0 && xE < matrix.length && yE >= 0 && yE < matrix[0].length)
-                {
-                    matrix[xE] [yE] = 0;
+            while (xE >= 0 && yE >= 0) {
+
+                if (xE >= 0 && xE < matrix.length && yE >= 0 && yE < matrix[0].length) {
+                    matrix[xE][yE] = 0;
                 }
+
                 xE--;
                 yE--;
             }
@@ -44,22 +50,26 @@ public class Main {
             int xI = ivoS[0];
             int yI = ivoS[1];
 
-            while (xI >= 0 && yI < matrix[1].length)
-            {
-                if (xI >= 0 && xI < matrix.length && yI >= 0 && yI < matrix[0].length)
-                {
+            while (xI >= 0 && yI < matrix[1].length) {
+
+                if (xI >= 0 && xI < matrix.length && yI >= 0 && yI < matrix[0].length) {
                     sum += matrix[xI][yI];
                 }
 
                 yI++;
                 xI--;
             }
-
-            command = scanner.nextLine();
         }
 
         System.out.println(sum);
+    }
 
-
+    private static void fillTheMatrix(int rows, int cols, int[][] matrix) {
+        int value = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = value++;
+            }
+        }
     }
 }
