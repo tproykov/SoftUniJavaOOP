a32a33f0b896a011c6a23ec096312e887267e334
tproykov <tproykov@gmail.com>
2025-02-16 13:54:11 +0000

    update

--- src/JO02Encapsulation/L04FirstAndReverseTeam/Main.java
+++ src/JO02Encapsulation/L04FirstAndReverseTeam/Main.java
@@ -1,10 +1,41 @@
 package JO02Encapsulation.L04FirstAndReverseTeam;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
-        
+        List<Person> players = new ArrayList<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String[] input = scanner.nextLine().split("\\s+");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            try {
+                Person person = new Person(firstName, lastName, age, salary);
+                players.add(person);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        Team team = new Team("Black Eagles");
+        for (Person player : players) {
+            team.addPlayer(player);
+        }
+
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
     }
 }
