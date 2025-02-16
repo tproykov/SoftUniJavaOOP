a9703c386a4e0886f882c8f1ed8c0dd43cf110da
tproykov <tproykov@gmail.com>
2025-02-16 13:27:05 +0000

    update

--- src/JO02Encapsulation/L03ValidationData/Main.java
+++ src/JO02Encapsulation/L03ValidationData/Main.java
@@ -1,4 +1,35 @@
 package JO02Encapsulation.L03ValidationData;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            try {
+                Person person = new Person(firstName, lastName, age, salary);
+                people.add(person);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        double bonus = Double.parseDouble(scanner.nextLine());
+
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person);
+        }
+    }
 }
