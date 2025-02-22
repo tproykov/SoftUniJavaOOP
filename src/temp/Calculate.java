90ad5d246713cc7dc32bb47ba1f5f260d8b60207
tproykov <tproykov@gmail.com>
2025-02-22 19:19:54 +0000

    update

--- src/temp/Calculate.java
+++ src/temp/Calculate.java
@@ -2,15 +2,24 @@ package temp;
 
 public class Calculate {
 
+    static int n;
+
     public static void main(String[] args) {
 
         int result = Calculate.cube(5);
 
         System.out.println(result);             // 125
 
+        System.out.println("n from main method:" + n);
+
     }
 
-    static int cube(int x) {
+    private static int cube(int x) {            //Static method
         return x * x * x;
+    }
+
+    static {
+        System.out.print("n from static block: ");
+        n = 10;
     }
 }
