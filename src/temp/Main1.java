7fe71a45963eb819403e85271877dcf909f66f33
tproykov <tproykov@gmail.com>
2025-02-22 19:25:36 +0000

    update

---
+++ src/temp/Main1.java
@@ -0,0 +1,14 @@
+package temp;
+
+public class Main1 {
+
+    static int n;
+    public static void main(String[] args) {
+        System.out.println("From main method: ");
+        System.out.println(n);
+    }
+    static {
+        System.out.println("From static block: ");
+        n = 10;
+    }
+}
