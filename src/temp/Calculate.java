391c91460b4d192cd29d7c60a9220c262b3f23f6
tproykov <tproykov@gmail.com>
2025-02-22 19:13:00 +0000

    Update Calculate.java

--- src/temp/Calculate.java
+++ src/temp/Calculate.java
@@ -2,15 +2,15 @@ package temp;
 
 public class Calculate {
 
-    static int cube(int x) {
-        return x * x * x;
-    }
-
     public static void main(String[] args) {
 
         int result = Calculate.cube(5);
 
         System.out.println(result);             // 125
-        System.out.println(Math.pow(2, 3));     // 8.0
+
+    }
+
+    static int cube(int x) {
+        return x * x * x;
     }
 }
