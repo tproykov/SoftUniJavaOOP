e5cb3071f15e95dea3c8c4d573253d4f4e2d7244
tproykov <tproykov@gmail.com>
2025-02-22 19:44:25 +0000

    update

--- src/temp/Main1.java
+++ src/temp/Main1.java
@@ -5,12 +5,11 @@ public class Main1 {
     static int n;                                   //Static (class) variable
 
     static {                                        //Static block implemented before the main method
-        System.out.println("From static block: ");
+        System.out.println("From static block: " + n);
         n = 10;
     }
 
     public static void main(String[] args) {
-        System.out.println("From main method: ");
-        System.out.println(n);
+        System.out.println("From main method: " + n);
     }
 }
