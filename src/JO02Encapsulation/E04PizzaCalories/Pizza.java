67711f11d6199352f19e3f5cff1453728d872d15
tproykov <tproykov@gmail.com>
2025-02-24 23:33:04 +0000

    update

--- src/JO02Encapsulation/E04PizzaCalories/Pizza.java
+++ src/JO02Encapsulation/E04PizzaCalories/Pizza.java
@@ -10,10 +10,44 @@ public class Pizza {
     private List<Topping> toppings;
 
     public Pizza(String name, int numberOfToppings) {
-
-
-
+        setName(name);
+        setToppings(numberOfToppings);
     }
 
+    private void setToppings(int numberOfToppings) {
+        if (numberOfToppings >= 0 && numberOfToppings <= 10) {
+            this.toppings = new ArrayList<>();
+        } else {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        }
+    }
 
+    private void setName(String name) {
+        if (name.trim().length() > 0 && name.length() <= 15) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
+        }
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addTopping(Topping topping) {
+        this.toppings.add(topping);
+    }
+
+    public double getOverallCalories() {
+        double totalCalories = 0;
+        for (Topping topping : this.toppings) {
+            totalCalories += topping.calculateCalories();
+        }
+        totalCalories += dough.calculateCalories();
+        return totalCalories;
+    }
 }
