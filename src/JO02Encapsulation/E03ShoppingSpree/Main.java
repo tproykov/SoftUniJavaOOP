e4dcdc6f570875b04269b5f9994266bfb2b12d01
tproykov <tproykov@gmail.com>
2025-02-24 18:23:37 +0000

    update

--- src/JO02Encapsulation/E03ShoppingSpree/Main.java
+++ src/JO02Encapsulation/E03ShoppingSpree/Main.java
@@ -1,4 +1,13 @@
 package JO02Encapsulation.E03ShoppingSpree;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
 }
