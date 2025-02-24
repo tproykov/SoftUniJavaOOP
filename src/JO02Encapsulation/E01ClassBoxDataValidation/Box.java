d532b8101d2dc1c6ccf8e5dd4718383f9883bf8f
tproykov <tproykov@gmail.com>
2025-02-24 18:21:41 +0000

    update

--- src/JO02Encapsulation/E01ClassBoxDataValidation/Box.java
+++ src/JO02Encapsulation/E01ClassBoxDataValidation/Box.java
@@ -2,5 +2,17 @@ package JO02Encapsulation.E01ClassBoxDataValidation;
 
 public class Box {
 
+    private int length;
+    private int width;
+    private int height;
+
+    public Box(int length, int width, int height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+
+
 
 }
--- src/JO02Encapsulation/E02AnimalFarm/Main.java
+++ src/JO02Encapsulation/E02AnimalFarm/Main.java
@@ -1,4 +1,18 @@
 package JO02Encapsulation.E02AnimalFarm;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Chicken chicken = new Chicken(name, age);
+
+        System.out.println(chicken);
+    }
 }
