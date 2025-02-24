6d9ebc21411b3d1fc5984568fb437345b7fef58a
tproykov <tproykov@gmail.com>
2025-02-24 21:24:42 +0000

    update

--- src/JO02Encapsulation/E01ClassBoxDataValidation/Box.java
+++ src/JO02Encapsulation/E01ClassBoxDataValidation/Box.java
@@ -2,17 +2,37 @@ package JO02Encapsulation.E01ClassBoxDataValidation;
 
 public class Box {
 
-    private int length;
-    private int width;
-    private int height;
+    private double length;
+    private double width;
+    private double height;
 
-    public Box(int length, int width, int height) {
+    public Box(double length, double width, double height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
 
+    private void setLength(double length) {
+        this.length = length;
+    }
 
+    private void setWidth(double width) {
+        this.width = width;
+    }
 
+    private void setHeight(double height) {
+        this.height = height;
+    }
 
+    public double calculateSurfaceArea() {
+        return 2 * length * width + 2 * height * width + 2 * length * height;
+    }
+
+    public double calculateLateralSurfaceArea() {
+        return 2 * height * width + 2 * length * height;
+    }
+
+    public double calculateVolume() {
+        return length * width * height;
+    }
 }
--- src/JO02Encapsulation/E01ClassBoxDataValidation/Main.java
+++ src/JO02Encapsulation/E01ClassBoxDataValidation/Main.java
@@ -8,6 +8,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int length = Integer.parseInt(scanner.nextLine());
+        int width = Integer.parseInt(scanner.nextLine());
+        int height = Integer.parseInt(scanner.nextLine());
 
+        Box box = new Box(length, width, height);
+
+        System.out.printf("Surface Area - %.2f\n", box.calculateSurfaceArea());
+        System.out.printf("Lateral Surface Area - %.2f\n", box.calculateLateralSurfaceArea());
+        System.out.printf("Volume - %.2f\n", box.calculateVolume());
     }
 }
