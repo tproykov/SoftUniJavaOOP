f0a153fc6c2e77bf077302b86d5e6aebab84a74e
tproykov <tproykov@gmail.com>
2025-02-24 21:57:15 +0000

    update

--- src/JO02Encapsulation/E01ClassBoxDataValidation/Main.java
+++ src/JO02Encapsulation/E01ClassBoxDataValidation/Main.java
@@ -12,10 +12,15 @@ public class Main {
         double width = Double.parseDouble(scanner.nextLine());
         double height = Double.parseDouble(scanner.nextLine());
 
-        Box box = new Box(length, width, height);
+        try {
+            Box box = new Box(length, width, height);
 
-        System.out.printf("Surface Area - %.2f\n", box.calculateSurfaceArea());
-        System.out.printf("Lateral Surface Area - %.2f\n", box.calculateLateralSurfaceArea());
-        System.out.printf("Volume - %.2f\n", box.calculateVolume());
+            System.out.printf("Surface Area - %.2f\n", box.calculateSurfaceArea());
+            System.out.printf("Lateral Surface Area - %.2f\n", box.calculateLateralSurfaceArea());
+            System.out.printf("Volume - %.2f\n", box.calculateVolume());
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
