bdf6f046111cb5f628191ef335e26edcf06b0b65
tproykov <tproykov@gmail.com>
2025-02-24 18:51:45 +0000

    update

--- src/JO02Encapsulation/E03ShoppingSpree/Product.java
+++ src/JO02Encapsulation/E03ShoppingSpree/Product.java
@@ -1,4 +1,36 @@
 package JO02Encapsulation.E03ShoppingSpree;
 
 public class Product {
+
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        setName(name);
+        setCost(cost);
+    }
+
+    private void setName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setCost(double cost) {
+        if (cost < 0) {
+            throw new IllegalArgumentException("Cost cannot be negative");
+        }
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+
 }
