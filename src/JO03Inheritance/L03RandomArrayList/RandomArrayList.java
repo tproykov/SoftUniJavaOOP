603cf3c0793658025c9ee0a7f6fd1648a0e41cc1
tproykov <tproykov@gmail.com>
2025-02-25 23:09:40 +0000

    Create RandomArrayList.java

---
+++ src/JO03Inheritance/L03RandomArrayList/RandomArrayList.java
@@ -0,0 +1,10 @@
+package JO03Inheritance.L03RandomArrayList;
+
+import java.util.ArrayList;
+
+public class RandomArrayList extends ArrayList {
+
+    public Object getRandomElement() {
+        return null;
+    }
+}
