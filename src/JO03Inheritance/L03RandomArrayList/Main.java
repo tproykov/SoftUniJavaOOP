06b23469df585816c8a6456a99eed918b2dd72bd
Todor Proykov <tproykov@gmail.com>
2025-03-02 21:30:52 +0200

    update

--- src/JO03Inheritance/L03RandomArrayList/Main.java
+++ src/JO03Inheritance/L03RandomArrayList/Main.java
@@ -1,4 +1,8 @@
 package JO03Inheritance.L03RandomArrayList;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/JO03Inheritance/L03RandomArrayList/RandomArrayList.java
+++ src/JO03Inheritance/L03RandomArrayList/RandomArrayList.java
@@ -1,10 +1,23 @@
 package JO03Inheritance.L03RandomArrayList;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class RandomArrayList extends ArrayList {
+public class RandomArrayList<T> extends ArrayList<T> {
 
-    public Object getRandomElement() {
-        return null;
+    public T getRandomElement() {
+
+        if (super.isEmpty()) {
+            return null;
+        }
+
+        Random random = new Random();
+
+        int index = random.nextInt(super.size());
+
+        T element = super.get(index);
+
+        super.remove(element);
+        return element;
     }
 }
