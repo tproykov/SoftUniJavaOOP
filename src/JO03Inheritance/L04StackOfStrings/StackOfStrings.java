3e8c8c75192c79984fe24caef39c45f5192bd2bb
Todor Proykov <tproykov@gmail.com>
2025-03-02 22:26:08 +0200

    update

--- src/JO03Inheritance/L04StackOfStrings/StackOfStrings.java
+++ src/JO03Inheritance/L04StackOfStrings/StackOfStrings.java
@@ -5,17 +5,21 @@ import java.util.ArrayList;
 public class StackOfStrings {
 
     private ArrayList<String> data;
+    
+    public StackOfStrings() {
+        this.data = new ArrayList<>();
+    }
 
     public void push(String item) {
         data.add(item);
     }
 
     public String pop() {
-        return data.removeLast();
+        return data.remove(data.size() - 1);
     }
 
     public String peek() {
-        return data.getLast();
+        return data.get(data.size() - 1);
     }
 
     public boolean isEmpty() {
