56d1069991ebc91352cc0c5c0aad943eb9442326
Todor Proykov <tproykov@gmail.com>
2025-03-02 22:42:30 +0200

    update

--- src/JO03Inheritance/L04StackOfStrings/StackOfStrings.java
+++ src/JO03Inheritance/L04StackOfStrings/StackOfStrings.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class StackOfStrings {
 
     private ArrayList<String> data;
-    
+
     public StackOfStrings() {
         this.data = new ArrayList<>();
     }
