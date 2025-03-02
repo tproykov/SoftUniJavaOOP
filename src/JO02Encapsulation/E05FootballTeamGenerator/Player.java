049af339d85e7d9833e6980e75fac2f009c95719
Todor Proykov <tproykov@gmail.com>
2025-03-02 18:32:01 +0200

    Update Player.java

--- src/JO02Encapsulation/E05FootballTeamGenerator/Player.java
+++ src/JO02Encapsulation/E05FootballTeamGenerator/Player.java
@@ -31,25 +31,37 @@ public class Player {
     }
 
     private void setEndurance(int endurance) {
-        if (endurance < 0) {
+        if (endurance < 0 || endurance > 100) {
             throw new IllegalArgumentException("Endurance should be between 0 and 100.");
         }
         this.endurance = endurance;
     }
 
     private void setSprint(int sprint) {
+        if (sprint < 0 || sprint > 100) {
+            throw new IllegalArgumentException("Sprint should be between 0 and 100.");
+        }
         this.sprint = sprint;
     }
 
     private void setDribble(int dribble) {
+        if (dribble < 0 || dribble > 100) {
+            throw new IllegalArgumentException("Dribble should be between 0 and 100.");
+        }
         this.dribble = dribble;
     }
 
     private void setPassing(int passing) {
+        if (passing < 0 || passing > 100) {
+            throw new IllegalArgumentException("Passing should be between 0 and 100.");
+        }
         this.passing = passing;
     }
 
     private void setShooting(int shooting) {
+        if (shooting < 0 || shooting > 100) {
+            throw new IllegalArgumentException("Shooting should be between 0 and 100.");
+        }
         this.shooting = shooting;
     }
 
