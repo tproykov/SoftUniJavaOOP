3d0272800e937358d6cad9668a8af2b52f36d9d0
Todor Proykov <tproykov@gmail.com>
2025-03-02 20:22:45 +0200

    Update Main.java

--- src/JO02Encapsulation/E05FootballTeamGenerator/Main.java
+++ src/JO02Encapsulation/E05FootballTeamGenerator/Main.java
@@ -1,5 +1,7 @@
 package JO02Encapsulation.E05FootballTeamGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,15 +10,23 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        List<Team> teams = new ArrayList<>();
+
         String input;
         while (!(input = scanner.nextLine()).equals("END")) {
-            String[] tokens = input.split(";");
-            switch (tokens[0]) {
-                case "Team":
-                    
-                    break;
 
+            String[] tokens = input.split(";");
+
+            String command = tokens[0];
+            String teamName = tokens[1];
+
+            switch (command) {
+                case "Team":
+                    Team team = new Team(teamName);
+                    teams.add(team);
+                    break;
                 case "Add":
+                    
                     break;
 
                 case "Remove":
