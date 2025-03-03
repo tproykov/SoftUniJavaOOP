faf970c5db7dd9a74357b26e12001603277760e4
Todor Proykov <tproykov@gmail.com>
2025-03-03 17:59:50 +0200

    update

--- src/JO03Inheritance/E04NeedForSpeed/SportCar.java
+++ src/JO03Inheritance/E04NeedForSpeed/SportCar.java
@@ -2,7 +2,10 @@ package JO03Inheritance.E04NeedForSpeed;
 
 public class SportCar extends Car {
 
+    private static final double DEFAULT_FUEL_CONSUMPTION = 10;
+
     public SportCar(double fuel, int horsePower) {
         super(fuel, horsePower);
+        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
     }
 }
