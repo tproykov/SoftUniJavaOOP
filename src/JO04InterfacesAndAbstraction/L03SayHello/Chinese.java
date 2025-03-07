047a9a156abfec489f1a9ff4893ebe32d3f8ef8b
Todor Proykov <tproykov@gmail.com>
2025-03-07 17:23:00 +0200

    update

--- src/JO04InterfacesAndAbstraction/L03SayHello/Chinese.java
+++ src/JO04InterfacesAndAbstraction/L03SayHello/Chinese.java
@@ -1,4 +1,20 @@
 package JO04InterfacesAndAbstraction.L03SayHello;
 
-public class Chinese {
+public class Chinese implements Person {
+
+    private String name;
+
+    public Chinese(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
 }
--- src/JO04InterfacesAndAbstraction/L03SayHello/Main.java
+++ src/JO04InterfacesAndAbstraction/L03SayHello/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
 
-
         persons.add(new Bulgarian("Peter"));
         persons.add(new European("Peter"));
         persons.add(new Chinese("Peter"));
