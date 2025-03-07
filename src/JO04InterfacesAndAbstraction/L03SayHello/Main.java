e1469267b59d0e9ba7ee5347a6440826850d3050
Todor Proykov <tproykov@gmail.com>
2025-03-07 17:28:59 +0200

    update

--- src/JO04InterfacesAndAbstraction/L03SayHello/Main.java
+++ src/JO04InterfacesAndAbstraction/L03SayHello/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+
         List<Person> persons = new ArrayList<>();
 
         persons.add(new Bulgarian("Peter"));
