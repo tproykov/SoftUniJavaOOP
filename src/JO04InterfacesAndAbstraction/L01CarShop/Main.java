56c4895f4e998406caa96e96c07889d10332e247
Todor Proykov <tproykov@gmail.com>
2025-03-07 09:07:00 +0200

    update

--- src/JO04InterfacesAndAbstraction/L01CarShop/Main.java
+++ src/JO04InterfacesAndAbstraction/L01CarShop/Main.java
@@ -4,16 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        public static void main (String[]args){
-            Car seat = new Seat("Leon", "gray", 110, "Spain");
+        Car seat = new Seat("Leon", "gray", 110, "Spain");
 
-            System.out.println(String.format(
-                    "%s is %s color and have %s horse power",
-                    seat.getModel(),
-                    seat.getColor(),
-                    seat.getHorsePower()));
-            System.out.println(seat.toString());
-        }
+        System.out.printf(
+                "%s is %s color and have %s horse power%n",
+                seat.getModel(),
+                seat.getColor(),
+                seat.getHorsePower());
 
+        System.out.println(seat);
     }
 }
--- src/JO04InterfacesAndAbstraction/L01CarShop/Seat.java
+++ src/JO04InterfacesAndAbstraction/L01CarShop/Seat.java
@@ -1,16 +1,41 @@
 package JO04InterfacesAndAbstraction.L01CarShop;
 
-public class Seat {
+public class Seat implements Car{
 
+    private String model;
+    private String color;
+    private int horsePower;
+    private String countryProduced;
+
+    public Seat(String model, String color, int price, String country) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = price;
+        this.countryProduced = country;
+    }
 
     @Override
     public String toString() {
         return null;
     }
 
+    @Override
+    public String getModel() {
+        return "";
+    }
 
+    @Override
+    public String getColor() {
+        return "";
+    }
 
+    @Override
+    public Integer getHorsePower() {
+        return 0;
+    }
 
-
-
+    @Override
+    public String countryProduced() {
+        return "";
+    }
 }
