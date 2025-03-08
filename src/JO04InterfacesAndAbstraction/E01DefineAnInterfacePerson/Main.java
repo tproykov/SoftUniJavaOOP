db8b679ce5bd3f67cbb5f5b39558524acf4b0940
Todor Proykov <tproykov@gmail.com>
2025-03-08 11:46:15 +0200

    update

--- src/JO04InterfacesAndAbstraction/E01DefineAnInterfacePerson/Main.java
+++ src/JO04InterfacesAndAbstraction/E01DefineAnInterfacePerson/Main.java
@@ -1,4 +1,14 @@
 package JO04InterfacesAndAbstraction.E01DefineAnInterfacePerson;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
 }
