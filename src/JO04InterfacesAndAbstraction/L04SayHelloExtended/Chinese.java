6775f6eac1d5d49b3f2926eea44a40f9a3e25ce8
Todor Proykov <tproykov@gmail.com>
2025-03-08 08:49:57 +0200

    update

--- src/JO04InterfacesAndAbstraction/L04SayHelloExtended/Chinese.java
+++ src/JO04InterfacesAndAbstraction/L04SayHelloExtended/Chinese.java
@@ -1,16 +1,9 @@
 package JO04InterfacesAndAbstraction.L04SayHelloExtended;
 
-public class Chinese implements Person {
-
-    private String name;
+public class Chinese extends BasePerson implements Person {
 
     public Chinese(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
+        super(name);
     }
 
     @Override
