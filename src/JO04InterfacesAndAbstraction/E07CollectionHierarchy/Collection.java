8dcb2c3d2ac1d6f46a28588b682d15ce87dae877
tproykov <tproykov@gmail.com>
2025-03-15 14:20:02 +0000

    update

--- src/JO04InterfacesAndAbstraction/E07CollectionHierarchy/Collection.java
+++ src/JO04InterfacesAndAbstraction/E07CollectionHierarchy/Collection.java
@@ -1,6 +1,11 @@
 package JO04InterfacesAndAbstraction.E07CollectionHierarchy;
 
+import java.util.List;
+
 public abstract class Collection {
+
+    private final int maxSize = 100;
+    protected List<String> items;
 
 
 
--- src/JO04InterfacesAndAbstraction/E07CollectionHierarchy/Main.java
+++ src/JO04InterfacesAndAbstraction/E07CollectionHierarchy/Main.java
@@ -3,7 +3,7 @@ package JO04InterfacesAndAbstraction.E07CollectionHierarchy;
 public class Main {
 
     public static void main(String[] args) {
-        
+
 
 
     }
