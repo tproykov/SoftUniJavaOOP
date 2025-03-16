341ea313a5280ff71fc42f7e0225d7f5763de909
tproykov <tproykov@gmail.com>
2025-03-16 13:33:56 +0000

    update

---
+++ src/JO05Polymorphism/E04Calculator/InputInterpreter.java
@@ -0,0 +1,25 @@
+package JO05Polymorphism.E04Calculator;
+
+public class InputInterpreter {
+    private CalculationEngine engine;
+
+    public InputInterpreter(CalculationEngine engine){
+        this.engine = engine;
+    }
+
+    public boolean interpret(String input) {
+        try {
+            engine.pushNumber(Integer.parseInt(input));
+        }catch (Exception ex){
+            engine.pushOperation(this.getOperation(input));
+        }
+        return true;
+    }
+    public Operation getOperation(String operation) {
+        if (operation.equals("*")) {
+            return new MultiplicationOperation();
+        }
+
+        return null;
+    }
+}
