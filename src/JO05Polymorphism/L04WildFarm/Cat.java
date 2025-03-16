1e99b8a8063954bf9245170a808e09b7ccc39537
tproykov <tproykov@gmail.com>
2025-03-16 08:23:33 +0000

    Update Cat.java

--- src/JO05Polymorphism/L04WildFarm/Cat.java
+++ src/JO05Polymorphism/L04WildFarm/Cat.java
@@ -2,10 +2,20 @@ package JO05Polymorphism.L04WildFarm;
 
 public class Cat extends Feline {
 
+    private String breed;
 
     public Cat(String animalName, String animalType, double animalWeight, Integer foodEaten,
-               String livingRegion) {
+               String livingRegion, String breed) {
         super(animalName, animalType, animalWeight, foodEaten, livingRegion);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
     }
 
     @Override
