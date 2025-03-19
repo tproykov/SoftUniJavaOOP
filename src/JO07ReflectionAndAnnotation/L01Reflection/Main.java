7bf49e53ec3a332f530d579fdfc025ab17780fed
tproykov <tproykov@gmail.com>
2025-03-19 21:27:28 +0000

    update

--- src/JO07ReflectionAndAnnotation/L01Reflection/Main.java
+++ src/JO07ReflectionAndAnnotation/L01Reflection/Main.java
@@ -1,4 +1,21 @@
 package JO07ReflectionAndAnnotation.L01Reflection;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
+
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+
+        Class reflectionClass = Reflection.class;
+
+        System.out.println(reflectionClass.getSimpleName());
+        System.out.println(reflectionClass.getSuperclass());
+
+        for (Class interfaceClass : reflectionClass.getInterfaces()) {
+            System.out.println(interfaceClass.getSimpleName());
+        }
+
+        Object object = (Reflection) reflectionClass.getDeclaredConstructor().newInstance();
+        System.out.println(object);
+    }
 }
--- src/JO13ExamPreparation/E04_19_Dec_2023/E041HighQualityStructure/src/main/java/climbers/Main.java
+++ src/JO13ExamPreparation/E04_19_Dec_2023/E041HighQualityStructure/src/main/java/climbers/Main.java
@@ -1,15 +1,15 @@
 package JO13ExamPreparation.E04_19_Dec_2023.E041HighQualityStructure.src.main.java.climbers;
 
-import climbers.core.Controller;
-import climbers.core.Engine;
-import climbers.core.EngineImpl;
+//import climbers.core.Controller;
+//import climbers.core.Engine;
+//import climbers.core.EngineImpl;
 
-public class Main {
-
-    public static void main(String[] args) {
-        //TODO
-//        Controller controller = new ControllerImpl();
-//        Engine engine = new EngineImpl(controller);
-//        engine.run();
-    }
-}
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        //TODO
+////        Controller controller = new ControllerImpl();
+////        Engine engine = new EngineImpl(controller);
+////        engine.run();
+//    }
+//}
