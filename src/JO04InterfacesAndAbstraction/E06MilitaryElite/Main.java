1474ebcf9ea8052400ea6864366cb25234441ebd
tproykov <tproykov@gmail.com>
2025-03-26 22:01:58 +0000

    Update Main.java

--- src/JO04InterfacesAndAbstraction/E06MilitaryElite/Main.java
+++ src/JO04InterfacesAndAbstraction/E06MilitaryElite/Main.java
@@ -1,9 +1,14 @@
 package JO04InterfacesAndAbstraction.E06MilitaryElite;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        
 
 
     }
